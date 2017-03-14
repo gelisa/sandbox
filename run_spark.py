48c903f4a7e5b121a9c678143b5c75961f455cae
E.A. Guseva <gelisa@users.noreply.github.com>
2017-03-13 23:17:31 -0400

    minor bug fix

--- run_spark.py
+++ run_spark.py
@@ -2,7 +2,7 @@
 from pyspark import SparkConf
 from pyspark import SparkContext
 import os
-import simple_app
+import spark_class_example as sp_example
 
 conf = SparkConf()
 sc = SparkContext(conf=conf)
@@ -13,6 +13,6 @@ for datafile in os.listdir(folder):
     a_list.append(os.path.join(folder,datafile))
 
 sp_filenames = sc.parallelize(a_list)
-sp_classes = sp_filenames.map(simple_app.Custom_class)
+sp_classes = sp_filenames.map(sp_example.Custom_class)
 line_counts = sp_classes.map(lambda x: x.count_lines())
 print(line_counts.reduce(lambda x,y: x+y))
